7cfdaf25834fd58263dfa8c73c058be91d0c4728
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-07-24 10:56:37 -0300

    NEW: multiply func tests

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(sum(1, 2, 3))
+	fmt.Println(multiply(10, 10))
 }
 
 func sum(i ...int) int {
@@ -11,6 +12,16 @@ func sum(i ...int) int {
 
 	for _, v := range i {
 		total += v
+	}
+
+	return total
+}
+
+func multiply(i ...int) int {
+	total := 1
+
+	for _, v := range i {
+		total *= v
 	}
 
 	return total
